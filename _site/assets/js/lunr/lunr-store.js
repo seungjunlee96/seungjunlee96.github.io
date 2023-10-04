e8cdd72d7ac451d49ce8d71c83928fcb909d0665
Seungjun Lee <lsjj096@gmail.com>
2023-10-04 14:26:46 +0900

    :sparkle: updated profile image and removed previous profile images

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "AnoGAN",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/anogan/",
+        "teaser": null
+      },{
+        "title": "Inception scores",
+        "excerpt":"Inception Score Papers Improved techniques for training GANs (https://arxiv.org/pdf/1606.03498.pdf) A Note on the Inception Score (https://arxiv.org/pdf/1801.01973.pdf) Evaluating (Black-Box) Generative Models In contrast to supervised learning, Generative Adversarial Networks lack an objective function that measures “What is realistic?”, which makes it difficult to compare performance of different models. Why is it...","categories": ["Deep","learning"],
+        "tags": [],
+        "url": "http://localhost:4000/deep/learning/inception-scores/",
+        "teaser": null
+      }]
